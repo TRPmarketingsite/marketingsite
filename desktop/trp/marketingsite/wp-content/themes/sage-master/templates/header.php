29538433678dea9a7bb2e24396d9e862f0df6169
Mauricio Cuba <mcubaalcon@gmail.com>
2015-12-10 16:17:18 -0400

    Config header and add mobile navbar funcionality

--- desktop/trp/marketingsite/wp-content/themes/sage-master/templates/header.php
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/templates/header.php
@@ -1,10 +1,21 @@
-<header class="banner">
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.jpg' ?>" alt="">
+      </a>
+    </div>
+
+    <nav class="collapse navbar-collapse pull-right" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
