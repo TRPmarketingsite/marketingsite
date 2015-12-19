48b6e3446e46a005e9c2e7bf9bf7b15c62b7d729
Mauricio Cuba <mcubaalcon@gmail.com>
2015-12-19 15:40:01 -0400

    Homepage styles and improvements

--- desktop/trp/marketingsite/wp-content/themes/sage-master/page-templates/home.php
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/page-templates/home.php
@@ -6,7 +6,7 @@
 	<div class="trp-home-masthead-bottom"></div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-5">
+			<div class="col-md-4">
 				<h1>
 					Find the right bed for your patient. <strong>The Right Place.</strong>
 					<a href="#" class="trp-button">
@@ -14,7 +14,7 @@
 					</a>
 				</h1>
 			</div>
-			<div class="col-md-7">
+			<div class="col-md-offset-1 col-md-7">
 				<img src="<?= get_stylesheet_directory_uri() ?> /assets/images/hero.png" alt="">
 			</div>
 		</div>
@@ -26,13 +26,13 @@
 		40% of patients discharged from the hospital require post-acute care
 	</h2>
 	<p>
-		Choosing the right facility can be an overwhelming process for the patient and family. It can also be challenging for providers. Previously, there was no centralized way to share, search and secure a bed in a post-acute care facility. Introducing <strong>The Right Place</strong>.
+		 Introducing <strong>The Right Place</strong>: a communication solution that empowers hospitals and post-acute providers to match the right patient in the right place of care quickly and efficiently.
 	</p>
 	<div class="row">
 		<div class="col-sm-4 col-xs-12 trp-features-hospitals">
 			<div class="trp-feature-helper"></div>
 			<div class="trp-feature">
-				<i></i>
+				<img src="<?= get_stylesheet_directory_uri() ?> /assets/images/1.jpg" alt="">
 				<h4>
 					HOSPITALS
 				</h4>
@@ -47,7 +47,7 @@
 		<div class="col-sm-4 col-xs-12 trp-features-facilities">
 			<div class="trp-feature-helper"></div>
 			<div class="trp-feature">
-				<i></i>
+				<img src="<?= get_stylesheet_directory_uri() ?> /assets/images/2.jpg" alt="">
 				<h4>
 					SKILLED NURSING FACILITIES
 				</h4>
@@ -62,7 +62,7 @@
 		<div class="col-sm-4 col-xs-12 trp-features-orgs">
 			<div class="trp-feature-helper"></div>
 			<div class="trp-feature">
-				<i></i>
+				<img src="<?= get_stylesheet_directory_uri() ?> /assets/images/3.jpg" alt="">
 				<h4>
 					Accountable Care Orgs
 				</h4>
@@ -86,12 +86,15 @@
 				</h2>
 				<ul>
 					<li>
+						<i class="icon-check-square-o"></i>
 						HIPAA compliant security protocols
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						State-of-the-art 256 bit encryption
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						SSL security
 					</li>
 				</ul>
@@ -115,21 +118,27 @@
 				</h2>
 				<ul>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Occupancy
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Referal volume
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Current mix of payer source
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Average length of stay
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Gender mix
 					</li>
 					<li>
+						<i class="icon-check-square-o"></i>
 						Case mix by clinical attribute 
 					</li>
 				</ul>
@@ -141,7 +150,7 @@
 
 <div class="trp-testimonials text-center">
 	<div class="container">
-		<!-- Dynamic functionality - Random -->
+		<i class="icon-quote"></i>
 		<h3 class="trp-testimonial-title">
 			This is the client testimonial space. This text will be replaced.
 		</h3>
