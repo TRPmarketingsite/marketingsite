2a21cb35289c77e6f6952bb2eeb625ba4794aa02
Mauricio Cuba <mcubaalcon@gmail.com>
2015-12-22 15:47:30 -0400

    About template

---
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/page-templates/about.php
@@ -0,0 +1,71 @@
+<?php
+	/* Template Name: About */
+?>
+
+<div class="trp-aco-hero trp-about-hero">
+	<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/about-hero.jpg" alt="" class="trp-aco-hero-img">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-11 text-left">
+				<h1>
+					About The Right Place
+				</h1>
+				<p>
+					The Right Place provides a more fluid and efficient process for service providers to identify and match patients to the right place of care in a simpler way.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="trp-mobile-app-analytics trp-aco-section trp-about-section trp-about-section-1">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/about-section-1.png" alt="">
+			</div>
+			<div class="col-md-6">
+				<h2>
+					It’s your Bed Board, we just made it easier to use
+				</h2>
+				<span>
+					Dr. Alan Abrams and Katherine Chambers began developing The Right Place in 2013 after Dr. Abrams recognized a particular pain point in how patients are transferred from hospitals to SNFs. Particularly, the discharge process, which took too long to get the patient to an available bed. With over 30 years of experience in geriatric medicine, Dr. Abrams has managed patient care in multiple settings from hospitals to nursing homes and home health services. Katherine comes to the partnership with a wealth of knowledge in developing online products from her time at HomeAway, Inc. and Starwood Hotels where she led first-mover initiatives in the travel space with the emergence of online travel agencies in the late 1990s.
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="trp-mobile-app trp-aco-section trp-about-section trp-about-section-2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>
+					There must be a better way
+				</h2>
+				<span>
+					After hours of phone calls, faxes, forms and voicemails, patient placement is still beholden to a clunky process. 
+					<br>
+					After all that time, over 24% of patients end up back in the hospital just to start that process over again. What if patients are not being placed in the right facility when they are discharged from a hospital and we could help shift that . . . while getting patients out of hospitals quicker, filling empty beds at skilled nursing facilities (SNFs) with the right patient mix, lowering readmissions rates to hospitals, and engaging patients in decision making of their post-acute care options?
+				</span>
+			</div>
+			<div class="col-md-6">
+				<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/about-section-2.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="trp-use-today">
+	<div class="container text-center">
+		<h2>
+			Start Using The Right Place Today
+		</h2>
+		<span>
+			It’s time to make your life easier.
+		</span>
+		<a href="#" class="trp-button">
+			Let’s Get Started
+		</a>
+	</div>
+</div>
