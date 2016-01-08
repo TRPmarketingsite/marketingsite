ccb9d4bf41527fad461476866bc95a45bb43a85c
Mauricio Cuba <mcubaalcon@gmail.com>
2016-01-07 20:05:13 -0400

    404 template updated

--- desktop/trp/marketingsite/wp-content/themes/sage-master/404.php
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/404.php
@@ -1,7 +1,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+<div class="container trp-404">
+	<h3 class="trp-404-title">
+		Sorry - this page either does not exist or has moved.
+	</h3>
 </div>
-
-<?php get_search_form(); ?>
