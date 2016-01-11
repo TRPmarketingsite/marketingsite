64a7270f31634e6471c8c016ad465fdc2f8f64d3
Mauricio Cuba <mcubaalcon@gmail.com>
2016-01-11 17:44:50 -0400

    Update on newsroom template

--- desktop/trp/marketingsite/wp-content/themes/sage-master/page-templates/newsroom.php
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/page-templates/newsroom.php
@@ -1,7 +1,7 @@
 <?php
 /* Template Name: Newsroom */
 
-$newsroomPosts = get_posts( array('posts_per_page' => 5) );
+$newsroomPosts = get_posts( array('posts_per_page' => 5, 'category_name' => 'News' ) );
 $latestNewsroomPosts = get_posts( array('posts_per_page' => 2, 'orderby' => 'date', 'order'=> 'DESC') );
 ?>
 
