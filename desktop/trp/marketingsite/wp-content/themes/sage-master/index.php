2c236e41ccd9a5a2fccc4892fdb0b22a42602684
Mauricio Cuba <mcubaalcon@gmail.com>
2016-01-13 15:10:19 -0400

    Exclude News category from blog

--- desktop/trp/marketingsite/wp-content/themes/sage-master/index.php
+++ desktop/trp/marketingsite/wp-content/themes/sage-master/index.php
@@ -8,6 +8,8 @@
 	  <?php get_search_form(); ?>
 	<?php endif; ?>
 
+	<?php query_posts('cat=-'.get_cat_ID('News')); ?>
+
 	<?php while (have_posts()) : the_post(); ?>
 	  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 	<?php endwhile; ?>
